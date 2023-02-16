b74adf1f91ed9bd6a94d710508b5c82f98b8c79b
faymso <faith.muthoni@assentcompliance.com>
2023-02-16 22:25:47 +0300

    Initial commit for projects done during a class undertaken.

---
+++ rate.py
@@ -0,0 +1,7 @@
+# Calculting pay by optaining the rate and the hours worked
+
+hours_worked = float(input("Enter Hours worked: "))
+rate_per_hour = float(input("Enter rate per hour: "))
+pay = hours_worked * rate_per_hour
+
+print(f"Your pay is {pay}")
---
+++ temperature.py
@@ -0,0 +1,7 @@
+# Taking temp in celsius then converting to fahrenheit
+
+celsius_temperature = float(input("Enter a temperature in celsius: "))
+
+fahrenheit_temperature = (celsius_temperature * 1.8) + 32
+
+print(f"The temperature is {fahrenheit_temperature} fahrenheit")
