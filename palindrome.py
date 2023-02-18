1f47d40be00e03e74f11c5ee966abd7a7156b205
faymso <faith.muthoni@assentcompliance.com>
2023-02-18 15:57:41 +0300

    Have finished on the sum of even numbers exercise.

--- palindrome.py
+++ palindrome.py
@@ -2,3 +2,7 @@
 Write a program that prompts the user for a string and checks whether the string is a palindrome. (i.e the string reads
 the same forward and backward.
 """
+
+
+def check_palindrome(palindrome):
+    for letter in palindrome:
--- sum.py
+++ sum.py
@@ -1,3 +1,21 @@
 """
 Write a program that takes in a list of integers and returns the sum of all even numbers in the list.
 """
+
+
+def sum_even_numbers(numbers):
+
+    sum = 0
+
+    for number in numbers:
+        if number % 2 == 0:
+            sum = sum + number
+
+    return sum
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+results = sum_even_numbers(numbers)
+print(results)
